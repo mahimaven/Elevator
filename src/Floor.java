7201c08dafb7c0ee5ee3b110e3c2c416da816b91
mahimaven <ramasubbu.maheswari@gmail.com>
2015-12-17 01:22:47 -0500

    Create Floor.java

---
+++ src/Floor.java
@@ -0,0 +1,21 @@
+package Floor;
+
+public class Floor {
+private int floornumber;
+public Floor(int f)
+{
+	setFloorNumber(f);
+
+}
+private void setFloorNumber( int floornumber) {
+	// TODO Auto-generated method stub
+	this.setFloornumber(floornumber);
+	
+}
+public int getFloornumber() {
+	return floornumber;
+}
+public void setFloornumber(int floornumber) {
+	this.floornumber = floornumber;
+}
+}
