6b4e5cc01bba3b9a33d2fef64528f70d02d089ca
mahimaven <ramasubbu.maheswari@gmail.com>
2015-12-16 23:42:13 -0500

    Commit 2

--- src/elevator/ElevatorImpl.java
+++ src/elevator/ElevatorImpl.java
@@ -1,6 +1,13 @@
 package elevator;
 
 public class ElevatorImpl implements elevator {
+private static long traveltime=100;
+private static long doortoggletime=200;
+private static long dooridletime=300;
+private static long idletime=500;
+private int currentFloor;
+private int defaultFloorNum;
+private int elevatorNumber;
 
 	public ElevatorImpl(int elevatorNum, int floorNum) {
 		// TODO Auto-generated constructor stub
@@ -9,7 +16,19 @@ public class ElevatorImpl implements elevator {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+try
+{
+	while(true){
+		if(dest.size()==0)
+		{
+			if(getCurrentFloor() != getDefaultFloor())
+			{
+				System.out.printf(%sElev %d timeout\n", Utilities.timetoString(),getElevatorNumber());"
+				synchronized(this);		
+			}
+		}
+	}
+}
 	}
 
 	@Override
